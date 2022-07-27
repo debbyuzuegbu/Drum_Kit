0e344bbb0d721a74fa6acbceab8db60824ef5b6b
debbyuzuegbu <debbyuzuegbu@gmail.com>
2022-07-27 02:59:43 +0100

    added image and sound to drum letters

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
 for (var i = 0; i<document.querySelectorAll(".drum").length; i++)
 
 document.querySelectorAll("button")[i].addEventListener("click", function () { 
-    alert("i got clicked!");
+    var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
+
 });
